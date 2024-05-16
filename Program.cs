7b1808c059c8d0de0a1cedd2b5f98f7ec0dce442
opilane <opilane@HP-CZC0359NP1>
2024-05-16 11:32:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = 0;
+
+        while (true)
+        {
+            Console.Write("sisesta num (1, 2, or 3): ");
+            string input = Console.ReadLine();
+
+            num = int.Parse(input) == 1 ? 10 : int.Parse(input) == 2 ? 20 : int.Parse(input) == 3 ? 30 : 0;
+
+            Console.WriteLine("tulemus on: " + num);
+
+            Console.Write("tahad edaneda? (y/n): ");
+            string response = Console.ReadLine();
+
+            if (response.ToLower() != "y")
+            {
+                break;
+            }
+        }
+    }
+}
